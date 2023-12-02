91195c6b59e7b38dae37d3bbace8928937405e7f
akmalnatha <akmalabhirama2002@gmail.com>
2023-12-02 22:21:57 +0700

    fix error

--- main.py
+++ main.py
@@ -3,12 +3,10 @@ from fastapi import FastAPI
 
 from routes.kereta import kereta
 from routes.tiket import tiket
-from routes.user import user
 from routes.auth import auth
 
 app = FastAPI()
 
 app.include_router(kereta)
 app.include_router(tiket)
-app.include_router(user)
 app.include_router(auth)
